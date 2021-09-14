1afc05f0b1a78ca8d94a94dd5b5c9f9c5e09b311
Steve Jones <steve.jones763@gmail.com>
2021-09-14 19:37:14 +0100

    Adding some common seeds.

--- lib/run.rb
+++ lib/run.rb
@@ -11,7 +11,8 @@ tub_seed = [
   GridCoordinate.new(1,1),
   GridCoordinate.new(2,0),
   GridCoordinate.new(2,2)
-  ]
+]
+
 beacon_seed = [
   GridCoordinate.new(0,0),
   GridCoordinate.new(0,1),
@@ -21,11 +22,67 @@ beacon_seed = [
   GridCoordinate.new(2,3),
   GridCoordinate.new(3,2),
   GridCoordinate.new(3,3)
-  ]
+]
 
-game.seed_game_grid(beacon_seed)
+i_column_seed = [
+  GridCoordinate.new(5,8),
+  GridCoordinate.new(5,9),
+  GridCoordinate.new(5,10),
+  GridCoordinate.new(6,8),
+  GridCoordinate.new(6,10),
+  GridCoordinate.new(7,8),
+  GridCoordinate.new(7,9),
+  GridCoordinate.new(7,10),
+  GridCoordinate.new(8,8),
+  GridCoordinate.new(8,9),
+  GridCoordinate.new(8,10),
+  GridCoordinate.new(9,8),
+  GridCoordinate.new(9,9),
+  GridCoordinate.new(9,10),
+  GridCoordinate.new(10,8),
+  GridCoordinate.new(10,9),
+  GridCoordinate.new(10,10),
+  GridCoordinate.new(11,8),
+  GridCoordinate.new(11,10),
+  GridCoordinate.new(12,8),
+  GridCoordinate.new(12,9),
+  GridCoordinate.new(12,10),
+]
+
+glider_seed = [
+  GridCoordinate.new(0,1),
+  GridCoordinate.new(1,2),
+  GridCoordinate.new(2,0),
+  GridCoordinate.new(2,1),
+  GridCoordinate.new(2,2),
+]
+
+pulsar_seed = [
+  GridCoordinate.new(5,3),
+  GridCoordinate.new(6,3),
+  GridCoordinate.new(7,3),
+
+  GridCoordinate.new(5,8),
+  GridCoordinate.new(6,8),
+  GridCoordinate.new(7,8),
+
+  GridCoordinate.new(3,5),
+  GridCoordinate.new(3,6),
+  GridCoordinate.new(3,7),
+
+  GridCoordinate.new(8,5),
+  GridCoordinate.new(8,6),
+  GridCoordinate.new(8,7),
+]
+
+
+game.seed_game_grid(pulsar_seed)
+
+
 loop do
   sleep 0.3
   puts game.run_game
   puts "".center(20, "-")
 end
+
+# puts game.get_printable_grid
